e0aa0f222bde1f0f714579723d11e611fe238d42
Panagiotis Kiriakopoulos <panagkpetr@gmail.com>
2023-05-24 21:29:01 +0300

    0.1

---
+++ src/main/java/com/gr/oxcode/cona/ConaApplication.java
@@ -0,0 +1,13 @@
+package com.gr.oxcode.cona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConaApplication.class, args);
+	}
+
+}
